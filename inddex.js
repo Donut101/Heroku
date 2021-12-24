0bbe1f49598eb4b62abf9e5de9b032b115acfd0a
Aisha Rafique <aieshaparyani@gmail.com>
2021-12-24 16:52:06 +0500

    add start

--- inddex.js
+++ inddex.js
@@ -2,7 +2,7 @@ const express = require("express")
 const { process } = require("ipaddr.js")
 const app = express()
 
-app.get("/",function(res,req){
+app.get("/",function(req,res){
     res.send("WORKIng!!!")
 })
 
